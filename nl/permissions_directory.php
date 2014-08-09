f5768b18752e1487cd545930282cde74c847deb6
David-NF <neurofibromatose@live.nl>
2014-08-09 20:40:56 +0200

    eerste vertaling, 2 onvertaalde variabelen
    Moet voor de laatste twee variabelen nog even kijken wat ze precies doen en waar de functie precies zit. Ben er ook nog niet helemaal uit of "Directory" vertaald moet worden... Ben nog maar net begonnen met testen van deze extensie

---
+++ nl/permissions_directory.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * permissions_directory [Dutch]
+ *
+ * @package language
+ * @version $Id$
+ * @copyright (c) 2011 http://www.phpbb-services.com
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACL_CAT_DIRECTORY'			=> 'Directory',
+
+	'ACL_M_DELETE_DIR'			=> 'Kan website verwijderen',
+	'ACL_M_DELETE_COMMENT_DIR'	=> 'Kan reacties verwijderen',
+	'ACL_M_EDIT_DIR'			=> 'Kan website wijzigen',
+	'ACL_M_EDIT_COMMENT_DIR'	=> 'Kan reacties wijzigen',
+	'ACL_U_COMMENT_DIR'			=> 'Kan reactie plaatsen (als reacties zijn toegestaan in de categorie)',
+	'ACL_U_DELETE_DIR'			=> 'Kan eigen links verwijderen',
+	'ACL_U_DELETE_COMMENT_DIR'	=> 'Kan eigen reacties verwijderen',
+	'ACL_U_EDIT_DIR'			=> 'Kan eigen links wijzigen',
+	'ACL_U_EDIT_COMMENT_DIR'	=> 'Kan eigen reacties wijzigen',
+	'ACL_U_SEARCH_DIR'			=> 'Kan zoeken in de directory',
+	'ACL_U_SUBMIT_DIR'			=> 'Can submit a website in the directory',
+	'ACL_U_VOTE_DIR'			=> 'Can vote for a website of the directory',
+));
